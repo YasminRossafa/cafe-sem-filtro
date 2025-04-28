96efc6f6e8662bce896413899a6a5b5913789382
Yasmin Rossafa <mimirossafa@gmail.com>
2025-04-28 14:12:54 -0300

    initial page with layout done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,141 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import React from "react";
+import { Helmet } from "react-helmet";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-const IndexPage: React.FC<PageProps> = () => {
+const Inicio = (): JSX.Element => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
+    <>
+      {/* Adiciona Material Symbols e fontes */}
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Inria+Sans&family=Zen+Antique&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+
+      <div className="bg-white flex flex-row justify-center w-full">
+        <div className="bg-white w-[1280px] relative">
+          <div className="absolute w-full min-h-screen top-0 left-0">
+            {/* Fundo escuro */}
+            <div className="absolute w-full h-[491px] top-[550px] left-0 bg-[#1e1a18]" />
+
+            {/* Seção de Valores */}
+            <div className="absolute w-[1184px] h-[361px] top-[580px] left-[50%] translate-x-[-50%]">
+              {/* Título */}
+              <div className="absolute w-[335px] top-0 left-[426px] font-zen text-[#f2c38b] text-[45px] leading-[70px] whitespace-nowrap text-center">
+                Nossos Valores
+              </div>
+
+              {/* Cards de Valores */}
+              <div className="absolute w-[313px] h-[145px] top-[90px] left-[15px]">
+                <span className="material-symbols-outlined absolute w-24 h-[95px] top-0 left-[125px] text-[#f2c38b] text-6xl">
+                  skillet
                 </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+                <div className="absolute w-[313px] top-[92px] left-0 font-bree text-[#f2c38b] text-xl leading-[70px] text-center">
+                  Métodos de Preparação Exclusivos
+                </div>
+              </div>
 
-export default IndexPage
+              <div className="absolute w-[313px] h-[145px] top-[90px] left-[439px]">
+                <span className="material-symbols-outlined absolute w-[74px] h-[95px] top-0 left-[114px] text-[#f2c38b] text-6xl">
+                  grain
+                </span>
+                <div className="absolute w-[313px] top-[92px] left-0 font-bree text-[#f2c38b] text-xl leading-[70px] text-center">
+                  Grãos Selecionados e Sustentáveis
+                </div>
+              </div>
 
-export const Head: HeadFC = () => <title>Home Page</title>
+              <div className="absolute w-[220px] h-[145px] top-[90px] left-[889px]">
+                <span className="material-symbols-outlined absolute w-[92px] h-[95px] top-0 left-[65px] text-[#f2c38b] text-6xl">
+                  connect_without_contact
+                </span>
+                <div className="absolute w-[220px] top-[92px] left-0 font-bree text-[#f2c38b] text-xl leading-[70px] text-center">
+                  Experiências Interativas
+                </div>
+              </div>
+
+              {/* Textos pequenos */}
+              <p className="absolute w-[340px] top-[238px] left-0 font-inria text-[#f2c38b] text-xl text-center leading-[25px]">
+                Oferecer métodos de preparo como prensa francesa, siphon e cold brew.
+              </p>
+
+              <p className="absolute w-[340px] top-[238px] left-[424px] font-inria text-[#f2c38b] text-xl text-center leading-[25px]">
+                Trabalhar com fornecedores locais que utilizam práticas de cultivo sustentável, garantindo grãos de alta qualidade e de comércio justo.
+              </p>
+
+              <p className="absolute w-[340px] top-[238px] left-[830px] font-inria text-[#f2c38b] text-xl text-center leading-[25px]">
+                Promover workshops de barista e degustações de café, onde os clientes podem aprender sobre a arte do café e experimentar a preparação de suas próprias bebidas.
+              </p>
+            </div>
+
+            {/* Fundo início */}
+            <img
+              className="absolute w-full h-[550px] top-0 left-0 object-cover"
+              alt="Fundo início"
+              src="/images/fundo-inicio.png"
+            />
+
+            {/* Logo */}
+            <img
+              className="absolute w-[282px] h-[158px] top-[290px] left-[230px] object-cover"
+              alt="Logo café sem filtro"
+              src="/images/logo.webp"
+            />
+
+            {/* Topo */}
+            <div className="absolute w-[1054px] h-[52px] top-[54px] left-[50%] translate-x-[-50%] flex justify-between items-center">
+              <div className="flex gap-4">
+                <div className="font-inria text-[#f2c38b] text-[40px]">SOBRE |</div>
+                <div className="font-inria text-[#f2c38b] text-[40px]">MENU |</div>
+                <div className="font-inria text-[#f2c38b] text-[40px]">CONTATO</div>
+              </div>
+
+              <a
+                style={{ color: "black" }}
+                className="relative w-[254px] h-[52px] bg-[#d4903e] flex items-center justify-center font-inria no-underline text-[32px]"
+                href="https://wa.me/5511991407988?text=Quero%20saber%20mais%20sobre%20a%20cafeteria%2C%20vi%20algumas%20informa%C3%A7%C3%B5es%20no%20site!"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                WhatsApp
+              </a>
+            </div>
+
+            {/* Texto Principal */}
+            <p className="absolute w-[435px] top-[198px] left-[785px] font-bold text-[#f2c38b] text-5xl text-center">
+              Métodos artesanais, grãos selecionados <br></br>
+              e uma experiência incrível!
+            </p>
+          </div>
+
+          {/* Rodapé */}
+          <div className="absolute w-full h-[162px] top-[1000px] left-0 bg-[#3f322b]">
+            <div className="relative w-[1089px] h-[72px] top-[37px] left-[50%] translate-x-[-50%] flex justify-between items-center">
+              <div className="flex items-center gap-2">
+                <span className="material-symbols-outlined text-[#f2c38b] text-4xl">call</span>
+                <div className="font-inria text-[#f2c38b] text-3xl">(11) 99140-7988</div>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <span className="material-symbols-outlined text-[#f2c38b] text-4xl">location_on</span>
+                <p className="font-inria text-[#f2c38b] text-3xl">Rua dos Grãos de Café - 12345-678</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <span className="material-symbols-outlined text-[#f2c38b] text-4xl">schedule</span>
+                <p className="font-inria text-[#f2c38b] text-3xl text-right">
+                  Seg à Sab<br />06h às 19h
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Inicio;
